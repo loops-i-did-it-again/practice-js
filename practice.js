528b9270522a92de16dca408cc4a128e0b80cdfa
dani <dani.zaghian@gmail.com>
2020-10-06 10:06:21 -0700

    adds js conversions

--- practice.js
+++ practice.js
@@ -129,4 +129,40 @@ function printSums(nums1, nums2) {
     });
   });
 }
-printSums([1, 5, 10], [100, 500, 1000]);
+// printSums([1, 5, 10], [100, 500, 1000]);
+
+// # Write a function that accepts an array of strings and returns a new array containing every other string from the original array.For example, if the input is["a", "b", "c", "d", "e", "f"], the output should be["a", "c", "e"].
+function selectEvenItems(strings) {
+  var result = [];
+  var index = 0;
+  strings.forEach(function (string) {
+    if (index % 2 === 0) {
+      result.push(string);
+    }
+    index++;
+  });
+  return result;
+}
+// console.log(selectEvenItems(["a", "b", "c", "d", "e", "f"]));
+
+// # Write a method that accepts one argument - an array of numbers.The method should return the greatest number.For example, if the input is[5, 4, 8, 1, 2], the output should be 8.
+function max(numbers) {
+  var currentMax = numbers[0];
+  numbers.forEach(function (number) {
+    if (number > currentMax) {
+      currentMax = number;
+    }
+  });
+  return currentMax;
+}
+// console.log(max([5, 4, 8, 1, 2]));
+
+// # Write a method that accepts a number and returns its factorial.For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+function factorial(number) {
+  var result = 1;
+  for (var i = number; i > 1; i--) {
+    result = result * i;
+  }
+  return result;
+}
+// console.log(factorial(5));
